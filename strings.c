618cff811bb236ce40520523ba601983e3d13975
Shismohammad <shismohammadmulla@gmail.com>
2022-03-07 13:42:43 +0530

    Fifth commit

--- strings.c
+++ strings.c
@@ -6,7 +6,7 @@ int main (void)
 
     int a,b;
     b= sizeof(a);
-    printf("Size of interger :%i bytes\n",b);//sizeof() gives size of datatype
+    printf("Size of interger :%i bytes\n",b);// sizeof() function gives size of datatype
 
     printf("%c \n", *s);
     printf("%c \n", *(s+1));
@@ -14,7 +14,7 @@ int main (void)
     printf("%c \n", *(s+3));
     printf("%c \n", *(s+4));
 
-    printf("\nThe null value :%i \n", *(s+13) );//This line will print nul value i.e 0. which indicates string end
+    printf("\nThe null value :%i \n", *(s+13) );// This line will print nul value i.e 0. which indicates string end
 
     printf("\n%s",s);
  
