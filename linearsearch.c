bad5184168ef73c758b1389c333d0d243cc91a13
Shismohammad <shismohammadmulla@gmail.com>
2022-03-19 15:20:52 +0530

    commit

--- linearsearch.c
+++ linearsearch.c
@@ -6,7 +6,7 @@ int main (void)
     
     for ( int i = 0; i < 7; i++)
     {
-        if (numbers[i] == 8)
+        if (numbers[i] == 4)
          {
             printf("Found that number");
             return 0;
