3f3a2e30756ac8293eaa707cfbaa6246429f1432
Shismohammad <shismohammadmulla@gmail.com>
2022-04-04 13:53:35 +0530

    commit

--- linearsearch.c
+++ linearsearch.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 
+//Programm to search a number from array
 int main (void)
 {
-    int numbers[] = {5,6,2,3,4,0,1,7};
+    int numbers[] = {5,6,2,3,4,0,1,7};//array declaration
     
     for ( int i = 0; i < 7; i++)
     {
         if (numbers[i] == 4)
          {
             printf("Found that number");
-            return 0;
-         }
-         
+            return 0; //Exit status and iff number found program will quit here not go to next line
+         } 
     }
     printf("Not found that number");
-    return 1;
-    
+    return 1; // Exit status 
 }
