0f1cdc7a956eb1262c4c296a72259ad709c55b31
Shismohammad <shismohammadmulla@gmail.com>
2022-04-08 11:28:50 +0530

    commit

--- structure.c
+++ structure.c
@@ -1,33 +1,32 @@
 #include <stdio.h>
 #include <string.h>
 
-//not compiling it need to be string // updated runing not verified.
+//phonebook using structure to keep related data togather
 typedef struct
 {   
-    char *name;
-    char *number;
+    char *name; //name need to be string
+    long long number; 
 }
 person; //structure name
 
 int main (void)
 {
-      person people[2];
+      person people[2]; // declaring people variable array of type person
 
       people[0].name = "brian" ;
-      people[0].number = "8354153136";
+      people[0].number = 8354153136;
 
       people[1].name = "david";
-      people[1].number = "9341355524";
+      people[1].number = 9341355524;
 
     for( int i=0 ; i<2 ; i++ )
     {
-        if ( strcmp (people[i].name , "david") == 0)
+        if ( strcmp (people[i].name , "david") == 0) //compare name with required contact name
         {
-            printf("contact found !\nThe number is :%i", people[i].number);
-            return 0;
+            printf("david contact found !\nThe number is :%lli", people[i].number);
+            return 0; //exit when found contact number
         }
     }
     printf("Not found");
-    return 1;
-
+    return 1; //exit status when not found any contact with name
 }
