302e7934dee566b78351230e219d9b648a2da695
Shismohammad <shismohammadmulla@gmail.com>
2022-04-10 16:49:09 +0530

    commit

--- address.c
+++ address.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 
+// program to see address of variable in hexadecimal and decimal
 int main(void)
 {   
     int n = 50;
-    
     int *p = &n ;// p is pointer variable
 
-    printf("%i \n", *p);//Prints Value of variable n
+    printf("Value of variable n : %i \n", *p);//Prints Value of variable n
+    printf("Address of variable n in hexadecimal format : %p \n", &n);//Prints memory location of pointer variable n
+    printf("Address of variable n with hex to decimal converted : %i \n", &n);// convert hexadecimal address to decimal
 
-    printf("%p \n", p);//Prints memory location of variable n
-
-    printf("%p \n", &p);//Prints memory location of pointer variable p
- 
 }
