c0dd91d265b59c763fc795a2510a301fe3487f98
Shismohammad <shismohammadmulla@gmail.com>
2022-04-11 15:18:12 +0530

    commit

--- pointers.c
+++ pointers.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
+// code for printing string location with using pointer
 int main (void)
 {
-    char s[6] = "HELLO!";
+    char *s = "HELLO!"; //char * is similar to string
 
-    printf("%p \n", &s[0]);
-    printf("%p \n", &s[1]);
+    printf("%p \n", s[0]);
+    printf("%p \n", s[1]);
     printf("here is null/zero character %i",s[6]);
 }
