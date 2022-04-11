5960fb49fe88b8340e1a63ae5f5860a86c7d4332
Shismohammad <shismohammadmulla@gmail.com>
2022-04-11 13:15:44 +0530

    commit

--- address.c
+++ address.c
@@ -6,7 +6,7 @@ int main(void)
     int n = 50;
     int *p = &n ;// p is pointer variable
 
-    printf("Value of variable n : %i \n", *p);//Prints Value of variable n
+    printf("Value of variable n : %i \n", *p);// *p say go to that address which stored in pointer p
     printf("Address of variable n in hexadecimal format : %p \n", &n);//Prints memory location of pointer variable n
     printf("Address of variable n with hex to decimal converted : %i \n", &n);// convert hexadecimal address to decimal
 
