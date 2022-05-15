43abb995b374abde68cd4545979c9b25b01ffdfc
Shismohammad <shismohammadmulla@gmail.com>
2022-05-15 12:58:24 +0530

    commit

--- filewrite.c
+++ filewrite.c
@@ -5,16 +5,15 @@
 int main(void)
 {
     FILE *fileptr;
+    fileptr = fopen("file.text","a");
     char string[100] = "hello world";
 
-    if (fileptr == NULL)
+    if (fileptr == NULL) // If file is not found it return 1.
     {
         return 1;
     }
-    
-    fileptr = fopen("file.text","w");
 
-    fprintf(fileptr,"%s",string);
+    fprintf(fileptr,"%s\n",string);
     printf("Write success");
     fclose(fileptr);
 }
