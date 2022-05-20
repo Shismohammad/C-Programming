98a731aea2765762402001d4578f68b4a83ad1c3
Shismohammad <shismohammadmulla@gmail.com>
2022-05-20 15:05:56 +0530

    commit

--- list.c
+++ list.c
@@ -1,43 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// code to resize list using malloc and realloc.
 int main(void)
 {
-    int *list= malloc(3*sizeof(int));//initializig pointer variable list and allocate 3byte multiplied by size of int (which is 1 )
+    int *list; //initializig pointer variable list
+    list = malloc(3*sizeof(int)); // allocate 3 byte multiplied by size of int
 
-    if(list==NULL) //check end of memory or problem with memory
+    if(list == NULL) //check end of memory or problem with memory
     { 
         return 1;
     }
 
-
-    list[0]=1;//any memory space allocated by malloc is also considerd as array because of back to back allocation
+    list[0]=1; //any memory space allocated by malloc is also considerd as array.
     list[1]=2;
     list[2]=3;
     
-    int *temp = malloc(4*sizeof(int));//initializig pointer variable temp allocate 3byte multiplied by size of int (which is 1 )
-    //also we can use realloc(__,__) function
-    if (temp==NULL) //check end of memory or problem with memory
-    {
+    int *temp = realloc(list,4*sizeof(int));
+
+    if (temp == NULL) //check end of memory or problem with memory
+    {   
+        free(list);
         return 1;
     }
-
-    for (int i=0; i<4; i++)
-    {
-        temp[i]=list[i];//adding value of list varible to temp variable
-    }
     
-    temp[3]=4;
+    temp[3] = 4;
  
-    free(list);
-
-    list = temp ;
-
-    free(temp); 
+    list = temp;
 
     for (int i = 0; i < 4; i++)
     {
         printf("%i\n",list[i]);
     }
-    
+        
+    free(list);
+    free(temp); 
 }
