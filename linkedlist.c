3bd3aa647bd419ba0e3ac92541497be471a965b3
Shismohammad <shismohammadmulla@gmail.com>
2022-05-27 17:27:48 +0530

    commit

--- linkedlist.c
+++ linkedlist.c
@@ -11,19 +11,20 @@ node;
 
 int main (void)
 {
-    node *list;
+    node *list; // creating list pointer variable which will points to linked list
     list = NULL;
 
-    node *n = malloc(sizeof(node));
+    node *n; // n is temporary pointer variable of type node
+    n = malloc(sizeof(node)); // allocating memory of size node to n pointer
     if (n == NULL)
     {
         return 1;
     }
     
-    n->number = 1;
-    n->next = NULL;
+    n->number = 1; // storing data 1 to number field in node 
+    n->next = NULL; // storing NULL to address field in node
 
-    list = n;
+    list = n; // storing address of node in list variable
 
     n = malloc(sizeof(node));
     if (n == NULL)
@@ -49,6 +50,20 @@ int main (void)
     n->next = NULL;
 
     list->next->next = n;
+
+    n = malloc(sizeof(node));
+    if (n == NULL)
+    {   
+        free(list->next->next);
+        free(list->next);
+        free(list);
+        return 1;
+    }
+
+    n->number = 4;
+    n->next = NULL;
+
+    list->next->next->next = n;
 
     printf("size of node : %d bytes\n",sizeof(node));
     for (node *temp = list; temp != NULL; temp = temp->next)
